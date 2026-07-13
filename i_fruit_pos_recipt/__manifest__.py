6966e23af621cf275eba68d161e802eee97ac70b
sameh <samehashraf947@gmail.com>
2026-07-13 16:13:24 +0300

    add file translate  Ar

--- i_fruit_pos_recipt/__manifest__.py
+++ i_fruit_pos_recipt/__manifest__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'I Fruit Pos Recipt',
-    'version': '16.0.0.1',
+    'version': '19.0.0.1',
     'category': 'Sales/Point of Sale',
     'summary': 'I Fruit Pos Recipt',
     'depends': ['point_of_sale'],
     'data': [],
     'assets': {
-        'point_of_sale.assets': [
-            "/i_fruit_pos_recipt/static/src/xml/OrderReceipt.xml",
-            "/i_fruit_pos_recipt/static/src/css/order_recipt.css",
+        'point_of_sale._assets_pos': [
+            'i_fruit_pos_recipt/static/src/app/screens/receipt_screen/receipt/**/*',
+            'i_fruit_pos_recipt/static/src/css/**/*',
         ],
     },
-    "installable": True,
-    "application": True,
-    "auto_install": False,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
     'external_dependencies': {},
 }
